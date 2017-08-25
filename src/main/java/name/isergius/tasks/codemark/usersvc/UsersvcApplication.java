3eae5b17a1aaa0518aea511ff7e3c282183ef494
Kondratyev Sergey <ron.rcf@gmail.com>
2017-08-25 12:50:42 +0500

    Create the project

---
+++ src/main/java/name/isergius/tasks/codemark/usersvc/UsersvcApplication.java
@@ -0,0 +1,12 @@
+package name.isergius.tasks.codemark.usersvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersvcApplication.class, args);
+	}
+}
---
+++ src/test/java/name/isergius/tasks/codemark/usersvc/UsersvcApplicationTests.java
@@ -0,0 +1,16 @@
+package name.isergius.tasks.codemark.usersvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsersvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
